d31741798f53fbfc33439f552743f825c2fc4eb8
Satanachrist94 <paul.yakusev@mail.ru>
2022-06-21 21:37:47 +0500

    Constraint Layout

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,24 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var likes: TextView? = null
+    var reposts: TextView? = null
+    var views :TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.post_list)
+        likes = findViewById(R.id.likes)
+        likes?.setText("500")
+
+        reposts = findViewById(R.id.reposts)
+        reposts?.setText("1k")
+
+        views = findViewById(R.id.views)
+        views?.setText("1k")
+
+
     }
 }
